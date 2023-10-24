356ce6d75b45956ec51cc48384fa1725ee7a1208
Chenna Kesava <chennakesava301@gmail.com>
2023-10-24 14:12:49 +0530

    init

---
+++ src/main/java/com/chenna/kafkafirst/KafkaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.chenna.kafkafirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFirstApplication.class, args);
+	}
+
+}
